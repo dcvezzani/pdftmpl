b6c6205e8321c2455a74275d7ef5703de811b1be
David Vezzani <david_vezzani@yahoo.com>
2015-06-13 09:55:19 -0700

    adds receta angular application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
   resources :work_weeks
   resources :invoices do
@@ -15,12 +14,13 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'welcome/index'
+  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
