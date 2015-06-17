2d3eb1cc5d742c9adaca140486e1dfd6e614b257
David Vezzani <david_vezzani@yahoo.com>
2015-06-16 22:47:59 -0700

    adds alerts to controller, updates test to include check for alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,7 @@
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
 //= require angular-rails-templates
+//= require angular-flash/dist/angular-flash
 //
 //= require_tree ./templates
 //
